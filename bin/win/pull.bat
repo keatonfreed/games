93db2ea34db52699834713221e30aa70e5f3ae5d
Keaton Freed <keatonafreed2@icloud.com>
2020-06-05 07:25:55 -0700

    $*

---
+++ bin/win/pull.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\keato\Documents\games
+git pull
+
---
+++ bin/win/versionSave.bat
@@ -0,0 +1,13 @@
+@echo off
+
+IF "%1"=="" GOTO no_args
+
+cd C:\Users\keato\Documents\games
+git add *
+git commit -a -m "$*"
+GOTO :end
+
+:no_args
+echo "versionSave Your commit message here"
+
+:end
